45bf84cc8f0cdbd6b69487177ddad532e81cb0a4
Kravz <41003193+Kravz@users.noreply.github.com>
2018-08-26 19:54:39 +0300

    Add files via upload

---
+++ MediaBlogEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\web_case_python\blog2\mediablog_project\mediablogenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
